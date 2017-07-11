6c988d9677ddbf872815cb6ea4a0e71522839590
Andrew Lee <leeas@stolaf.edu>
2017-07-11 16:27:24 -0500

    Windows install + clean/ant build task scripts

---
+++ Windows/clean.bat
@@ -0,0 +1,3 @@
+%USERPROFILE%\eclipsec --nosplash --launcher.suppressErrors -application org.eclipse.jdt.apt.core.aptBuild -data "c:\Users\leeas\workspace"
+
+ant -buildfile %USERPROFILE%\git\SwingJS\buildSite.xml
---
+++ Windows/installwindows.bat
@@ -0,0 +1,13 @@
+bitsadmin /transfer eclipse /priority high http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/neon/3/eclipse-java-neon-3-win32-x86_64.zip %USERPROFILE%\neon.zip
+bitsadmin /transfer unziplicense /priority high https://raw.githubusercontent.com/SwingJS/SwingJSUtils/master/Windows/Unzip_License %USERPROFILE%\Unzip_License
+bitsadmin /transfer unzip /priority high http://stahlworks.com/dev/unzip.exe %USERPROFILE%\unzip.exe
+start notepad "Unzip_License"
+
+unzip neon.zip
+
+del neon.zip
+
+bitsadmin /transfer j2s /priority high https://github.com/SwingJS/SwingJSUtils/raw/master/3.1.1.zip %USERPROFILE%\eclipse\dropins\3.1.1.zip
+unzip %USERPROFILE%\eclipse\dropins\3.1.1.zip -d %USERPROFILE%\eclipse\dropins\
+del %USERPROFILE%\eclipse\dropins\3.1.1.zip
+del %USERPROFILE%\unzip.exe
