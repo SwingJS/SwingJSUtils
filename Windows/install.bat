3731058fefaef99ce2ead8eec235097c5cfaee5a
Andrew Lee <leeas@stolaf.edu>
2017-07-11 11:03:03 -0500

    added unzip BSD license as listed as a distributors responsibility in their user license

--- Windows/install.bat
+++ Windows/install.bat
@@ -1,7 +1,10 @@
-powershell -Command "& {new-object System.Net.WebClient; $client.DownloadFile("https://hithub.com/BobHanson/java2script/releases/downloadv3.1.1/3.1.1.zip", "3.1.1.zip"); $client.DownloadFile("https://mirror.csclub.uwaterloo.ca./eclipse/technology/epp/downloads/release/neon/R/eclipse-jee-R-win32-x86_64.zip", "oxygen.zip"); $client.DownloadFile("http://stahlworks.com/dev/unzip.exe", "unzip.exe");}"
+powershell -Command "& {new-object System.Net.WebClient; $client.DownloadFile("https://hithub.com/BobHanson/java2script/releases/downloadv3.1.1/3.1.1.zip", "3.1.1.zip"); $client.DownloadFile("https://mirror.csclub.uwaterloo.ca./eclipse/technology/epp/downloads/release/neon/R/eclipse-jee-R-win32-x86_64.zip", "oxygen".zip"); $client.DownloadFile("http://stahlworks.com/dev/unzip.exe", "unzip.exe");}"
 unzip 3.1.1.zip
-unzip oxygen.zip
+unzip neon.zip
 move net.j2s.core_3.1.1.jar eclipse\dropins\net.j2s.core_3.1.1.jar
 del *.jar
 rmdir net.sf.j2s.ajax_3.1.1
 rmdir net.sf.j2s.lib_3.1.1
+del 3.1.1.zip
+del neon.zip
+del unzip.exe
