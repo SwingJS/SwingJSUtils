aec2e8c695d518e394c7a3635499a963fb367d2b
Andrew Lee <leeas@stolaf.edu>
2017-07-11 11:26:41 -0500

    added download sequence and open for the License

--- Windows/install.bat
+++ Windows/install.bat
@@ -1,7 +1,7 @@
-powershell -Command "& {(New_object System.Net.WebClient).DownloadFile("https://github.com/BobHanson/java2script/releases/downloadv3.1.1/3.1.1.zip", "3.1.1.zip")}"
-powershell -Command "& {(New_object System.Net.WebClient).DownloadFile("https://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/neon/R/eclipse-jee-R-win32-x86_64.zip", "oxygen.zip")}"
-powershell -Command "& {(New_object System.Net.WebClient).DownloadFile("http://stahlworks.com/dev/unzip.exe", "unzip.exe")}"
-powershell -Command "& {(New_object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SwingJS/SwingJSUtils/master/Windows/Unzip_License", "Unzip_License")}"
+powershell -Command "& {(New_object System.Net.WebClient).DownloadFile("https://github.com/BobHanson/java2script/releases/downloadv3.1.1/3.1.1.zip", "3.1.1.zip");}"
+powershell -Command "& {(New_object System.Net.WebClient).DownloadFile("https://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/neon/R/eclipse-jee-R-win32-x86_64.zip", "oxygen.zip");}"
+powershell -Command "& {(New_object System.Net.WebClient).DownloadFile("http://stahlworks.com/dev/unzip.exe", "unzip.exe");}"
+powershell -Command "& {(New_object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SwingJS/SwingJSUtils/master/Windows/Unzip_License", "Unzip_License");}"
 start notepad "Unzip_License"
 unzip 3.1.1.zip
 unzip neon.zip
