2be2778fc9a74c25118bdd06fe0b43331c6e752f
Andrew Lee <leeas@stolaf.edu>
2017-07-14 16:38:15 -0500

    Fix Windows Install Script

--- Windows/installwindows.bat
+++ Windows/installwindows.bat
@@ -2,11 +2,23 @@ bitsadmin /transfer unziplicense /priority high https://raw.githubusercontent.co
 bitsadmin /transfer unzip /priority high http://stahlworks.com/dev/unzip.exe %USERPROFILE%\unzip.exe
 start notepad "Unzip_License"
 
-
 :CheckOS
-IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
+java -d64 -version 2>&1
+IF errorlevel 1 GOTO 32BIT
+
+bitsadmin /transfer eclipse /priority high http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/neon/3/eclipse-java-neon-3-win32-x86_64.zip %USERPROFILE%\neon.zip
+unzip neon.zip
+del neon.zip
+bitsadmin /transfer j2s /priority high https://github.com/SwingJS/SwingJSUtils/raw/master/3.1.1.zip %USERPROFILE%\eclipse\dropins\3.1.1.zip
+unzip %USERPROFILE%\eclipse\dropins\3.1.1.zip -d %USERPROFILE%\eclipse\dropins\
+del %USERPROFILE%\eclipse\dropins\3.1.1.zip
+del %USERPROFILE%\unzip.exe
+GOTO END
 
 :32BIT
+where java >nul 2>&1
+IF errorlevel 1 GOTO  NOJAVA
+
 bitsadmin /transfer eclipse /priority high http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/neon/R/eclipse-jee-neon-R-win32.zip %USERPROFILE%\neon.zip
 unzip neon.zip
 
@@ -18,18 +30,8 @@ del %USERPROFILE%\eclipse\dropins\3.1.1.zip
 del %USERPROFILE%\unzip.exe
 GOTO END
 
-:64BIT
-bitsadmin /transfer eclipse /priority high http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/neon/3/eclipse-java-neon-3-win32-x86_64.zip %USERPROFILE%\neon.zip
-unzip neon.zip
+:NOJAVA
 
-del neon.zip
-
-bitsadmin /transfer j2s /priority high https://github.com/SwingJS/SwingJSUtils/raw/master/3.1.1.zip %USERPROFILE%\eclipse\dropins\3.1.1.zip
-unzip %USERPROFILE%\eclipse\dropins\3.1.1.zip -d %USERPROFILE%\eclipse\dropins\
-del %USERPROFILE%\eclipse\dropins\3.1.1.zip
-del %USERPROFILE%\unzip.exe
+echo No Java Installed. Please install Java first
 GOTO END
-
 :END
-
-
