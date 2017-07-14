91e3470ae1dbabfd423a1fe235710bc47ac54bf2
Andrew Lee <leeas@stolaf.edu>
2017-07-14 15:31:38 -0500

    Windows Install Script Update
    Added ability for the script to differentiate between x86_64 x86 architectures and to download the relevant binaries

--- Windows/installwindows.bat
+++ Windows/installwindows.bat
@@ -1,8 +1,13 @@
-bitsadmin /transfer eclipse /priority high http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/neon/3/eclipse-java-neon-3-win32-x86_64.zip %USERPROFILE%\neon.zip
 bitsadmin /transfer unziplicense /priority high https://raw.githubusercontent.com/SwingJS/SwingJSUtils/master/Windows/Unzip_License %USERPROFILE%\Unzip_License
 bitsadmin /transfer unzip /priority high http://stahlworks.com/dev/unzip.exe %USERPROFILE%\unzip.exe
 start notepad "Unzip_License"
 
+
+:CheckOS
+IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
+
+:32BIT
+bitsadmin /transfer eclipse /priority high http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/neon/R/eclipse-jee-neon-R-win32.zip %USERPROFILE%\neon.zip
 unzip neon.zip
 
 del neon.zip
@@ -11,3 +16,20 @@ bitsadmin /transfer j2s /priority high https://github.com/SwingJS/SwingJSUtils/r
 unzip %USERPROFILE%\eclipse\dropins\3.1.1.zip -d %USERPROFILE%\eclipse\dropins\
 del %USERPROFILE%\eclipse\dropins\3.1.1.zip
 del %USERPROFILE%\unzip.exe
+GOTO END
+
+:64BIT
+bitsadmin /transfer eclipse /priority high http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/neon/3/eclipse-java-neon-3-win32-x86_64.zip %USERPROFILE%\neon.zip
+unzip neon.zip
+
+del neon.zip
+
+bitsadmin /transfer j2s /priority high https://github.com/SwingJS/SwingJSUtils/raw/master/3.1.1.zip %USERPROFILE%\eclipse\dropins\3.1.1.zip
+unzip %USERPROFILE%\eclipse\dropins\3.1.1.zip -d %USERPROFILE%\eclipse\dropins\
+del %USERPROFILE%\eclipse\dropins\3.1.1.zip
+del %USERPROFILE%\unzip.exe
+GOTO END
+
+:END
+
+
